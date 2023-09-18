66714ade24b8108051bde785cb8e000dce78d4a1
xyzyx <xzyx99@gmail.com>
2023-09-17 23:35:45 -0700

    Adding some stuff for formatting in lsp

--- lsp-config.lua
+++ lsp-config.lua
@@ -4,38 +4,38 @@ local lsp = require('lsp-zero')
 lsp.preset({})
 
 lsp.ensure_installed({
-	'tsserver',
-	'eslint',
 	'lua_ls',
-	--'sumneko_lua',
-	'rust_analyzer',
 	'clangd',
+	-- 'tsserver',
+	-- 'eslint',
+	--'sumneko_lua',
+	--'rust_analyzer',
 })
 
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  sources = {
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-  },
-  mapping = {
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-  }
+	sources = {
+		{ name = 'nvim_lsp' },
+		{ name = 'nvim_lua' },
+	},
+	mapping = {
+		['<C-f>'] = cmp_action.luasnip_jump_forward(),
+		['<C-b>'] = cmp_action.luasnip_jump_backward(),
+	}
 })
 
-local cmp_select = {behavior =cmp.SelectBehavior.Select}
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({select = true}),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
 	['<C-Space>'] = cmp.mapping.complete(),
 })
 
 lsp.set_preferences({
-	sign_icons = { }
+	sign_icons = {}
 })
 
 lsp.setup_nvim_cmp({
@@ -43,8 +43,8 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({buffer = bufnr})
-	local opts = {buffer = bufnr, remap = false}
+	lsp.default_keymaps({ buffer = bufnr })
+	local opts = { buffer = bufnr, remap = false }
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
@@ -56,6 +56,11 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+	--
+	-- - [x] (LSP)   Format source on <A-S-F>
+	vim.keymap.set({ "n", "v" }, "<A-S-f>", ":LspZeroFormat<CR>")
+	--
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
--- settings.lua
+++ settings.lua
@@ -2,13 +2,14 @@ if not vim.g.vscode then
 	--- HMM are there actual settings i want to use for vscode then ?
 	vim.keymap.set("n", "<C-u>", "<C-u>zz")
 	vim.keymap.set("n", "<C-d>", "<C-d>zz")
-	vim.g.fileformat="unix"
-	vim.o.colorcolumn="80,120"
-	vim.o.cursorline=true
+	vim.g.fileformat = "unix"
+	vim.o.colorcolumn = "80,120"
+	vim.o.cursorline = true
 	vim.o.relativenumber = true
 	vim.o.number = true
 end
 
-vim.keymap.set({'n', 'v'}, "<leader>y", "\"+y")
-vim.keymap.set({'n', 'v'}, "<leader>p", "\"+p")
+vim.keymap.set({ 'n', 'v' }, "<leader>y", "\"+y")
+vim.keymap.set({ 'n', 'v' }, "<leader>p", "\"+p")
 vim.keymap.set("n", "<leader>s", ":w<CR>:so %<CR>")
+
