e594457a3cbf86fb1c29515335f65c7a2ed723e9
Nick Gridinskiy <nick.gridinskiy@creditkarma.com>
2023-11-01 17:12:31 -0700

    Fun new command to play with

--- settings.lua
+++ settings.lua
@@ -8,17 +8,20 @@ if not vim.g.vscode then
 	vim.keymap.set("n", "<C-i>", "<C-i>zz")
 	vim.keymap.set("n", "n", "nzz")
 	vim.keymap.set("n", "N", "Nzz")
-	vim.keymap.set("n", "<leader>e", ":Explore<CR>")
+	vim.keymap.set("n", "<leader>e", ":Hex<CR>")
 	vim.keymap.set("n", "<leader>E", ":Sex!<CR>")
 	vim.keymap.set("n", "<leader>m", ":marks<CR>")
+	-- vim.api.nvim_create_user_command("CopyRelPath", "call setreg('+', expand('%'))", {}) 
+	-- Copy relative file path
+	vim.keymap.set("n", "<leader>fy", ":call setreg('+', expand('%'))<CR>")
 	vim.g.fileformat = "unix"
 	vim.o.colorcolumn = "80,120"
 	vim.o.cursorline = true
 	vim.o.relativenumber = true
 	vim.o.number = true
+	vim.g.netrw_liststyle= 3 -- tree style listings by default
 end
 
 vim.keymap.set({ 'n', 'v' }, "<leader>y", "\"+y")
 vim.keymap.set({ 'n', 'v' }, "<leader>p", "\"+p")
 vim.keymap.set("n", "<leader>s", ":w<CR>:so %<CR>")
-
