379e0e730596b837203bb65b263edb4144ae3b77
XyZyX <xzyx99@gmail.com>
2024-02-28 20:27:19 -0800

    Removing hop from my setup.
    Ive never actually used it, and built in searching is much more powerful
    anyway

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -27,25 +27,3 @@ configs.setup {
     },
   },
 }
-
-local hop = require('hop')
-
-hop.setup({})
-local directions = require('hop.hint').HintDirection
-vim.keymap.set('', '<leader>f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, match_mappings = {} })
-  vim.cmd("norm zz")
-end, { remap = true })
-vim.keymap.set('', '<leader>F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, match_mappings = {} })
-  vim.cmd("norm zz")
-end, { remap = true })
-vim.keymap.set('', '<leader>t', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1, match_mappings = {} })
-  vim.cmd("norm zz")
-end, { remap = true })
-vim.keymap.set('', '<leader>T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1, match_mappings = {} })
-  vim.cmd("norm zz")
-end, { remap = true })
-
