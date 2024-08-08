07a154e52f8963b0d4d819a1763cd3f724abba2f
XyZyX <xzyx99@gmail.com>
2024-08-07 19:19:00 -0700

    Randomizing the waifu background

---
+++ lua/wezterm.lua
@@ -0,0 +1,71 @@
+-- Pull in the wezterm API
+local wezterm                       = require 'wezterm'
+
+-- This will hold the configuration.
+local config                        = wezterm.config_builder()
+local io                            = require 'io'
+local os                            = require 'os'
+local math                          = require 'math'
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme                 = 'GruvboxDark'
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar            = false
+
+
+local hsb = {
+  -- Darken the background image
+  brightness = 0.08,
+
+  -- You can adjust the hue by scaling its value.
+  -- a multiplier of 1.0 leaves the value unchanged.
+  hue = 1.0,
+
+  -- You can adjust the saturation also.
+  saturation = 1.0,
+}
+
+
+-- Path to your waifu directory
+local waifu_dir = os.getenv("HOME") .. "/Pictures/Waifu"
+
+-- Function to get a random file from the waifu directory
+local function get_random_waifu()
+  local files = {}
+  for file in io.popen('ls "' .. waifu_dir .. '"'):lines() do
+    table.insert(files, file)
+  end
+  if #files == 0 then
+    return nil
+  end
+  local random_file = files[math.random(#files)]
+  return waifu_dir .. '/' .. random_file
+end
+
+config.background = {
+  -- This is the deepest/back-most layer. It will be rendered first
+  {
+    source = {
+      File =
+      -- '/home/djmittens/Pictures/Waifu/wallhaven-j82veq.jpg'
+      -- '/home/djmittens/Pictures/Waifu/wallhaven-expqmk.jpg'
+      -- '/home/djmittens/Pictures/Waifu/684896_49040378423_o.png'
+          get_random_waifu()
+    },
+    -- The texture tiles vertically but not horizontally.
+    -- When we repeat it, mirror it so that it appears "more seamless".
+    -- An alternative to this is to set `width = "100%"` and have
+    -- it stretch across the display
+    repeat_x = 'Mirror',
+    hsb = hsb,
+    -- When the viewport scrolls, move this layer 10% of the number of
+    -- pixels moved by the main viewport. This makes it appear to be
+    -- further behind the text.
+    attachment = 'Fixed',
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ workspace-switcher.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env /usr/bin/python
+import re
+import sys
+import subprocess as subp
+
+# these are the physical dimensions of the monitor
+# in millimeters, which i guess is used for dpi calculations
+WIDTH_mm = 1190
+HEIGHT_mm = 340
+
+def xrandr(*args):
+    args = list(args)
+    args.insert(0, "xrandr")
+    res = subp.run(args, capture_output=True)
+    return str(res.stdout, "utf8")
+
+monitors = re.findall(r"\s\d\:\s([^\s]+)\s.+", xrandr("--listactivemonitors"))
+mon, width_px, height_px = re.findall(r"(\w+)\sconnected\sprimary\s(\w+)x(\w+)\+", xrandr())[0]
+
+
+if __name__ == "__main__":
+    print(mon, width_px, height_px)
+
+    for m in monitors:
+        print("Deleting user monitor: ", m)
+        xrandr("--delmonitor", m)
+
+    layout = sys.argv[1]
+
+    if(layout == "dual"):
+        print("Using dual layout")
+        wp = int(width_px)  // 2
+        hp = int(height_px)
+        wm = WIDTH_mm // 2
+        hm = HEIGHT_mm
+        xrandr("--setmonitor", f"{mon}-1", f"{wp}/{wm}x{hp}/{hm}+0+0", mon)
+        xrandr("--setmonitor", f"{mon}-2", f"{wp}/{wm}x{hp}/{hm}+{wp}+0", "none")
+    elif(layout == "stardeck"):
+        print("Using startdeck layout")
+        wp = int(width_px)  // 2
+        wps = wp // 2
+        hp = int(height_px)
+        wm = WIDTH_mm // 2
+        wms = wm // 2
+        hm = HEIGHT_mm
+        xrandr("--setmonitor", f"{mon}-1", f"{wp}/{wm}x{hp}/{hm}+{wps}+0", mon)
+        xrandr("--setmonitor", f"{mon}-2", f"{wps}/{wms}x{hp}/{hm}+{wps + wp}+0", "none")
+        xrandr("--setmonitor", f"{mon}-3", f"{wps}/{wms}x{hp}/{hm}+0+0", "none")
+    else:
+        print(f"Unknown layout {layout}, clearing display")
