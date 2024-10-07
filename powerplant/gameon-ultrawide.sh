50623bccbfe2c04a225984d260105a861270394c
XyZyX <xzyx99@gmail.com>
2024-10-07 01:39:48 -0700

    Some random modifications i guess.
    There was something weird happening with newer neovim, that changed
    behavior around setrel

--- powerplant/gameon-ultrawide.sh
+++ powerplant/gameon-ultrawide.sh
@@ -5,8 +5,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pkill -9 picom
 
 # Disable composition pipeline and enable G-sync/Freesync
-nvidia-settings --assign CurrentMetaMode="DP-4: 5120x1440_240 -5120+0 {ForceCompositionPipeline=Off, AllowGSYNCCompatible=On}, HDMI-0: 2560x1440_120 +0+0  {ForceCompositionPipeline=Off}"
+# nvidia-settings --assign CurrentMetaMode="DP-4: 5120x1440_240 -5120+0 {ForceCompositionPipeline=Off, AllowGSYNCCompatible=On}, HDMI-0: 2560x1440_120 +0+0  {ForceCompositionPipeline=Off}"
 
+nvidia-settings --assign CurrentMetaMode="DP-4: 2560x1440_240 -5120+0 {ForceCompositionPipeline=Off, AllowGSYNCCompatible=On}, HDMI-0: 2560x1440_120 +0+0  {ForceCompositionPipeline=Off}"
 $SCRIPT_DIR/workspace-switcher.py none
 #Disable 2º screen
 xrandr --output HDMI-0 --off
