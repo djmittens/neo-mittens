dc75ed6365cc579506fc9b720868ddb3f4fb3803
XyZyX <xzyx99@gmail.com>
2025-04-19 00:57:01 -0700

    Configuring dap to use telescope

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -14,19 +14,6 @@ dap.listeners.before.event_exited.dapui_config = function()
   dapui.close()
 end
 
-dap.adapters.codelldb = {
-  type = 'server',
-  port = "${port}",
-  executable = {
-    -- CHANGE THIS to your path!
-    command = vim.fn.expand('$HOME/bin/codelldb/adapter/codelldb'),
-    args = {"--port", "${port}"},
-
-    -- On windows you may have to uncomment this:
-    -- detached = false,
-  }
-}
-
 -- Make K be dap hover when enabled
 local api = vim.api
 local keymap_restore = {}
@@ -56,15 +43,28 @@ dap.listeners.after['event_terminated']['me'] = function()
       )
     elseif keymap.callback then
       vim.keymap.set(
-      keymap.mode,
-      keymap.lhs,
-      keymap.callback,
-      { buffer = keymap.buffer, silent = keymap.silent == 1 }
+        keymap.mode,
+        keymap.lhs,
+        keymap.callback,
+        { buffer = keymap.buffer, silent = keymap.silent == 1 }
       )
     end
   end
   keymap_restore = {}
 end
+
+dap.adapters.codelldb = {
+  type = 'server',
+  port = "${port}",
+  executable = {
+    -- CHANGE THIS to your path!
+    command = vim.fn.expand('$HOME/bin/codelldb/adapter/codelldb'),
+    args = { "--port", "${port}" },
+
+    -- On windows you may have to uncomment this:
+    -- detached = false,
+  }
+}
 
 dap.adapters.gdb = {
   type = "executable",
@@ -72,18 +72,6 @@ dap.adapters.gdb = {
   args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
 }
 
-dap.configurations.c = {
-  {
-    name = "Launch",
-    type = "gdb",
-    request = "launch",
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = "${workspaceFolder}",
-    stopAtBeginningOfMainSubprogram = false,
-  },
-}
 
 dap.configurations.cpp = {
   {
@@ -94,7 +82,7 @@ dap.configurations.cpp = {
     --   return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     -- end,
     program = vim.fn.getcwd() .. "/build/bin/hello-opengl",
-   cwd = '${workspaceFolder}',
+    cwd = '${workspaceFolder}',
     stopOnEntry = false,
   },
 }
@@ -116,5 +104,44 @@ dap.configurations.scala = {
     metals = {
       runType = "testTarget",
     },
+  },
+}
+
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+
+local function telescope_select_exec()
+  return coroutine.create(function(coro)
+    local opts = {}
+    pickers
+        .new(opts, {
+          prompt_title = "Path to executable",
+          finder = finders.new_oneshot_job({ "fd", "--hidden", "--no-ignore", "--type", "x", "", "build/", "bin/" }, {}),
+          sorter = conf.generic_sorter(opts),
+          attach_mappings = function(buffer_number)
+            actions.select_default:replace(function()
+              actions.close(buffer_number)
+              coroutine.resume(coro, action_state.get_selected_entry()[1])
+            end)
+            return true
+          end,
+        })
+        :find()
+  end)
+end
+
+dap.configurations.c = {
+  {
+    name = "Launch an executable",
+    type = "gdb",
+    request = "launch",
+    program = telescope_select_exec,
+    cwd = "${workspaceFolder}",
+    stopAtBeginningOfMainSubprogram = false,
   },
 }
